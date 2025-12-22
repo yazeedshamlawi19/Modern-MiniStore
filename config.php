277a4b75c9399ccac86b2ce6262fb4868a1c855d
yazeedshamlawi19 <yazeedshamlawi19@gmail.com>
2025-12-22 15:44:34 +0200

    config.php

--- config.php
+++ config.php
@@ -14,12 +14,7 @@ define('BASE_URL', 'http://localhost/mini_market_project/');
 
 date_default_timezone_set('Asia/Hebron');
 
-define('ADMIN_USER', 'admin');
-
-define('ADMIN_PASS', '1234');
 
 
-// Google OAuth
-define('GOOGLE_CLIENT_ID', '655360320158-8qmqog44mdgaf622hisrp5amdpq2bobi.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'http://localhost/mini_market_project/google_callback.php');
+
+
